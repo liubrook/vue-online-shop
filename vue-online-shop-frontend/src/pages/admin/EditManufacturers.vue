c836df81d1e432cb5497e977b68f4f45d7f3ac51
brookliu <492525217@qq.com>
2020-11-17 22:44:15 +0800

    干掉 mutation-types 硬编码

---
+++ vue-online-shop-frontend/src/pages/admin/EditManufacturers.vue
@@ -0,0 +1,36 @@
+<template>
+
+</template>
+
+<script>
+import ManufacturerForm from '@/components/ManufacturerForm'
+export default {
+  components: {
+    'manufacturer-form': ManufacturerForm
+  },
+  created() {
+    this.$store.dispatch('manufacturerById', {
+      manufacturerId: this.$route.params['id']
+    })
+  },
+  computed: {
+    model () {
+      const manufacturer = this.$store.getters.manufacturerById(this.$route.params['id'])
+
+      // 这里返回product的拷贝，是为了在修改product的拷贝之后，在保存之前不修改本地Vuex stire的product属性
+      return { ...manufacturer }
+    }
+  },
+  methods: {
+    addManufacturer(model) {
+      this.$store.dispatch('updateManufacturer', {
+        manufacturer: model
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-online-shop-frontend/src/pages/admin/NewManufacturers.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+import ManufacturerForm from '@/components/ManufacturerForm'
+export default {
+  components: {
+    'manufacturer-form': ManufacturerForm
+  },
+  computed: {
+    model() {
+      return {}
+    }
+  },
+  methods: {
+    addManufacturer(model) {
+      this.$store.dispatch('addManufacturer', {
+        manufacturer: model
+      })
+    }
+  }
+}
+</script>
