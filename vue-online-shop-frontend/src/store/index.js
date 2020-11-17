283368c788475969f24c861547cb9f23b1c9d1db
brookliu <492525217@qq.com>
2020-11-17 17:08:41 +0800

    抽出 Getters 和 Mutations 逻辑

--- vue-online-shop-frontend/src/store/index.js
+++ vue-online-shop-frontend/src/store/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios'
 
+import { productGetters, manufacturerGetters } from './getters';
+import { productMutations, cartMutations, manufacturerMutations } from './mutation';
+
 const API_BASE = 'http://localhost:3000/api/v1'
 
 Vue.use(Vuex);
@@ -16,51 +19,19 @@ export default new Vuex.Store({
     manufacturers: []
   },
   mutations: {
-    ADD_TO_CART(state, payload) {
-      const { product } = payload
-      state.cart.push(product)
-    },
-    REMOVE_FROM_CART(state, payload) {
-      const { productId } = payload
-      state.cart = state.cart.filter(product => product._id !== productId)
-    },
-    ALL_PRODUCTS(state) {
-      state.showLoader = true
-    },
-    ALL_PRODUCTS_SUCCESS(state, payload) {
-      const { products }  = payload
-
-      state.showLoader = false
-      state.products = products
-    },
-    PRODUCT_BY_ID(state) {
-      state.showLoader = true
-    },
-    PRODUCT_BY_ID_SUCCESS(state, payload) {
-      state.showLoader = false
-
-      const { product } = payload
-      state.product = product
-    }
+    ...productMutations,
+    ...cartMutations,
+    ...manufacturerMutations
   },
   getters: {
-    allProducts(state) {
-      return state.products
-    },
-    productById: (state, getters) => id => {
-      if (getters.allProducts.length > 0) {
-        return getters.allProducts.filter(p => p._id == id)[0]
-      } else {
-        return state.product
-      }
-    }
+    ...productGetters,
+    ...manufacturerGetters
   },
   actions: {
     allProducts({ commit }) {
       commit('ALL_PRODUCTS')
 
       axios.get(`${API_BASE}/products`).then(response => {
-        console.log('response', response)
         commit('ALL_PRODUCTS_SUCCESS', {
           products: response.data
         })
@@ -73,6 +44,37 @@ export default new Vuex.Store({
       axios.get(`${API_BASE}/products/${productId}`).then(response => {
         commit('PRODUCT_BY_ID_SUCCESS', {
           product: response.data
+        })
+      })
+    },
+    removeProduct({ commit }, payload) {
+      commit('REMOVE_PRODUCT')
+
+      const { productId } = payload
+      axios.delete(`${API_BASE}/products/${productId}`).then(() => {
+        // 返回productId，用户删除本地对应的商品
+        commit('REMOVE_PRODUCT_SUCCESS', {
+          productId
+        })
+      })
+    },
+    allManufacturers({ commit }) {
+      commit('ALL_MANUFACTURERS')
+
+      axios.get(`${API_BASE}/manufacturers`).then(response => {
+        commit('ALL_MANUFACTURERS_SUCCESS', {
+          manufacturers: response.data
+        })
+      })
+    },
+    removeManufacturer({ commit }, payload) {
+      commit('REMOVE_MANUFACTURER')
+
+      const { manufacturerId } = payload
+      axios.delete(`${API_BASE}/manufacturers/${manufacturerId}`).then(() => {
+        // 返回manufacturerId，用于删除本地对应的制造商
+        commit('REMOVE_MANUFACTURER_SUCCESS', {
+          manufacturerId
         })
       })
     }
